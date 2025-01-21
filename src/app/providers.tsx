e237edafa2a9f060061a96cf38dc392ad76e221c
hellno <hellno@users.noreply.github.com>
2025-01-21 15:38:30 +0100

    add posthog

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -3,13 +3,13 @@
 import dynamic from "next/dynamic";
 import type { Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
-import posthog from 'posthog-js';
-import { PostHogProvider } from 'posthog-js/react';
+import posthog from "posthog-js";
+import { PostHogProvider } from "posthog-js/react";
 
-if (typeof window !== 'undefined') {
+if (typeof window !== "undefined" && process.env.NEXT_PUBLIC_POSTHOG_KEY) {
   posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
     api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
-    person_profiles: 'identified_only',
+    person_profiles: "identified_only",
   });
 }
 
@@ -22,16 +22,17 @@ export function CSPostHogProvider({ children }: { children: React.ReactNode }) {
   return <PostHogProvider client={posthog}>{children}</PostHogProvider>;
 }
 
-export function Providers({ session, children }: { 
+export function Providers({
+  session,
+  children,
+}: {
   session: Session | null;
   children: React.ReactNode;
 }) {
   return (
     <CSPostHogProvider>
       <SessionProvider session={session}>
-        <WagmiProvider>
-          {children}
-        </WagmiProvider>
+        <WagmiProvider>{children}</WagmiProvider>
       </SessionProvider>
     </CSPostHogProvider>
   );
